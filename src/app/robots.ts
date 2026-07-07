5681b74e28461a7a5c4d908df8ee39615790afa1
mohamadreza121 <mrheidarpoor7@gmail.com>
2026-07-06 21:46:20 -0400

    Remove nonstandard robots host directive

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -21,7 +21,6 @@ export default function robots(): MetadataRoute.Robots {
         ]
       }
     ],
-    sitemap: `${siteConfig.url}/sitemap.xml`,
-    host: siteConfig.url
+    sitemap: `${siteConfig.url}/sitemap.xml`
   };
 }
